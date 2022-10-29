f1022f73f6d2563b88404b23834944e724132148
Tatyana Kapina <deniscapin@yandex.ru>
2022-10-29 11:29:43 +0300

    catalog

--- src/index.js
+++ src/index.js
@@ -2,3 +2,35 @@ import './index.html';
 import './card.html';
 import './cart.html';
 import './index.scss';
+
+
+
+import Swiper, {Thumbs, Scrollbar} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+
+const thumbSwiper = new Swiper ('.card__slider-thumb', {
+    spaceBetween: 44,
+    slidesPerView: 3,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+    modules: [Scrollbar]
+});
+
+new Swiper('.card__img', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    thumbs: {
+        swiper: thumbSwiper,
+        slideThumbActiveClass: 'card__thumb-btn_active',
+    },
+    modules: [Thumbs]
+});
+
+new Swiper('.recommended__carousel',{
+    spaceBetween: 30,
+    slidesPerView: 4,
+});
+
