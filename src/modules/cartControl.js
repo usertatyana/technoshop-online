c91f6c877ba16b3b836834240a82fc1e5dce85fd
Tatyana Kapina <deniscapin@yandex.ru>
2022-12-17 22:03:53 +0300

    cartcontrol,index.js

--- src/modules/cartControl.js
+++ src/modules/cartControl.js
@@ -1,30 +1,28 @@
 const addToToCart = (id, count = 1) => {
   const cartGoods = localStorage.getItem('cart-ts') ?
-    JSON.parse(localStorage.getItem('cart-ts')):
-{};
+    JSON.parse(localStorage.getItem('cart-ts')) :
+    {};
 
   cartGoods[id] = count;
 
   localStorage.setItem('cart-ts', JSON.stringify(cartGoods))
 };
 
-const removeToCart = (id, count) => {
+const removeToCart = (id) => {
   const cartGoods = localStorage.getItem('cart-ts') ?
-    JSON.parse(localStorage.getItem('cart-ts')):
-  {};
+    JSON.parse(localStorage.getItem('cart-ts')) :
+    {};
 
   delete cartGoods[id];
-
- cartGoods[id] = count;
 
   localStorage.setItem('cart-ts', JSON.stringify(cartGoods))
 };
 
-const checkItems = (classDelete) => {
-    const elems = document.querySelectorAll('[data-id-goods]');
+const checkItem = (classDelete) => {
+  const elems = document.querySelectorAll('[data-id-goods]');
 
   const cartGoods = localStorage.getItem('cart-ts') ?
-    JSON.parse(localStorage.getItem('cart-ts')):
+    JSON.parse(localStorage.getItem('cart-ts')) :
     {};
 
   let count = 0;
@@ -36,15 +34,15 @@ const checkItems = (classDelete) => {
   const cartElem = document.querySelector('.header__cart');
   cartElem.dataset.count = count;
 
-  if (!classDelete) {
-    const elems = document.querySelectorAll('[data-id-goods]');
+  if (classDelete) {
+    const elem = document.querySelectorAll('[data-id-goods]');
 
-    elems.forEach(elem =>{
-      if (cartGoods[elems.dataset.idGoods]) {
+    elems.forEach(elem => {
+      if (cartGoods[elem.dataset.idGoods]) {
         elem.classList.add(classDelete);
         elem.textContent = 'В корзине';
-      }else {
-        elem.classList.remove(classDelete);
+      } else {
+        elem.classList.add(classDelete);
         elem.textContent = 'В корзину';
       }
     })
@@ -52,7 +50,7 @@ const checkItems = (classDelete) => {
 };
 
 export const cartControl = ({wrapper, classAdd, classDelete}) => {
-  checkItems(classDelete);
+  checkItem(classDelete);
 
   if (wrapper) {
     wrapper.addEventListener('click', (e) => {
@@ -61,14 +59,13 @@ export const cartControl = ({wrapper, classAdd, classDelete}) => {
 
       if (!id) return;
 
-      if (target.closest('${classDelete}')) {
+      if (target.closest(`.${classDelete}`)) {
         removeToCart(id);
-      }else if (target.closest('.${classAdd}')) {
-        addToCart(id);
+      } else if (target.closest(`.${classAdd}`)) {
+        addToToCart(id);
       }
 
-      checkItems(classDelete);
-
+      checkItem(classDelete);
     })
   }
 }
